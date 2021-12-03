ec173f5626ff9349e81d904dd8af55aea009461c
wilcard-spec <dsamsonov@protonmail.com>
2021-12-03 22:31:11 +0100

    Update ResourcesTimeAssigner.cs

--- Assets/Scripts/ResourcesTimeAssigner.cs
+++ Assets/Scripts/ResourcesTimeAssigner.cs
@@ -11,16 +11,20 @@ public class ResourcesTimeAssigner : MonoBehaviour
     public Text ExpensesText;
     public DateTime date = DateTime.Now;
     public string sDate;
-    public int expenses;
+    public int expenses { get; set; }
+    public int balance { get; set; }
     public bool isPaused = false;
     Coroutine timer;
     public float timeUnit = 2f;
     // Start is called before the first frame update
     void Start()
     {
+        balance = 10000;
+        expenses = 200;
         BalanceText = GameObject.Find("FieldBalance").GetComponent(typeof(Text)) as Text;
         CurrentDate = GameObject.Find("FieldDate").GetComponent(typeof(Text)) as Text;
         ExpensesText = GameObject.Find("FieldMonthExpenses").GetComponent(typeof(Text)) as Text;
+        
         //  var balanceText = GameObject.Find("FieldBalance");
         //   balanceText.text = "123";
         timer = StartCoroutine(Timer());
@@ -47,7 +51,8 @@ public class ResourcesTimeAssigner : MonoBehaviour
 
        
         
-        BalanceText.text = "123";
+        BalanceText.text = balance.ToString();
+        ExpensesText.text = expenses.ToString();
     }
 
     IEnumerator Timer(float countTime = 1f)
@@ -58,20 +63,15 @@ public class ResourcesTimeAssigner : MonoBehaviour
             yield return new WaitForSeconds(countTime);
             date = date.AddMonths(1);
             CurrentDate.text = date.ToString();
+            balance -= expenses;
             Debug.Log("This one is from coroutine");
             Debug.Log(date.ToString());
         }
     }
 
-    public int GetBalance()
-    {
-        return Convert.ToInt32(BalanceText.text);
-    }
 
-    public void SetBalance(int amount)
-    {
-        BalanceText.text = amount.ToString();
-    }
+
+
 
     public void StopTime(Coroutine t)
     {
